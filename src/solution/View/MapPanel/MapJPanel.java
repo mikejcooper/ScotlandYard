1fbe14c1203e8ccd6f976149d160aa4ebe2a15cc
maartie0 <gebruiker@eduroam-216-033.nomadic.bris.ac.uk>
2015-03-19 16:17:29 +0000

    merge this shit

--- src/solution/View/MapPanel/MapJPanel.java
+++ src/solution/View/MapPanel/MapJPanel.java
@@ -1,5 +1,7 @@
 package solution.View.MapPanel;
 
+import scotlandyard.Graph;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -14,29 +16,32 @@ public class MapJPanel extends JPanel {
 
 
     private BufferedImage image;
-    private Set<ButtonHolder> buttons;
+//    private Set<ButtonHolder> buttons;
 
     public MapJPanel() {
-        PositionReader positionReader = new PositionReader();
-        try{positionReader.read();}
-        catch(IOException e){
-            System.exit(1);
-        }
-        this.buttons = positionReader.getButtonSet();
-        setPreferredSize(new Dimension(500,400));
-        URL u = this.getClass().getResource("map.jpg");
-        ImageIcon icon = new ImageIcon(u);
-        add(new JLabel(icon));
+//        PositionReader positionReader = new PositionReader();
+//        try{positionReader.read();}
+//        catch(IOException e){
+//            System.exit(1);
+//        }
+//        this.buttons = positionReader.getButtonSet();
+//        setPreferredSize(new Dimension(500,400));
+//        URL u = this.getClass().getResource("map.jpg");
+//        ImageIcon icon = new ImageIcon(u);
+//        add(new JLabel(icon));
+        GraphDisplay graphDisplay = new GraphDisplay();
+        add(graphDisplay.getMap());
+
 
     }
 
-    public void paintComponent(Graphics g0){
-        super.paintComponent(g0);
-        Graphics2D g = (Graphics2D) g0;
-        //g.drawImage(image,0,0,null);
-        for(ButtonHolder buttonHolder:buttons){
-            g.fillOval(buttonHolder.getxCoordinate(),buttonHolder.getyCoordinate(),10,10);
-        }
-    }
+//    public void paintComponent(Graphics g0){
+//        super.paintComponent(g0);
+//        Graphics2D g = (Graphics2D) g0;
+//        //g.drawImage(image,0,0,null);
+//        for(ButtonHolder buttonHolder:buttons){
+//            g.fillOval(buttonHolder.getxCoordinate(),buttonHolder.getyCoordinate(),10,10);
+//        }
+//    }
 
 }
