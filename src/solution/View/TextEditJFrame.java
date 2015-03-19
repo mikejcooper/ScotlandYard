45d6a2611a7fe3170a588fae5200d535c51a48dd
Michael Cooper <mikejcooper1@gmail.com>
2015-03-19 18:30:06 +0000

    index on master: e617d55 button ghosting

---
+++ src/solution/View/TextEditJFrame.java
@@ -0,0 +1,21 @@
+package solution.View;
+
+import javax.swing.*;
+
+/**
+ * Created by MikeCooper on 19/03/15.
+ */
+public class TextEditJFrame extends JFrame {
+
+
+   public TextEditJFrame() {
+
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setSize(600, 400);
+
+
+
+
+       setVisible(true);
+   }
+}
